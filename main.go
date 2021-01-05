c4b822c1c277592c13e04aa8dc21b42aeff5260c
Nator Verinumbe <natverior1@gmail.com>
2021-01-05 18:05:31 +0100

    testing out basic code

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
-)
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+
+	)
+
+func helloWorld(w http.ResponseWriter, req *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("authentification Microservice"))
+}
+
+func handleRequest(){
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/", helloWorld)
+	log.Fatal(http.ListenAndServe(":8001",myRouter))
+}
 
 func main(){
 	fmt.Println("authentification Microservice")
+
 }
